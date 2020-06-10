75af57c55694e4e250451064bbc5a66f9ec1bffa
Jeff Bai <j.bai@wustl.edu>
2020-06-10 04:35:26 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,17 +1,3 @@
 class Appointment
-  attr_accessor :date, :patient, :doctor
-
-  @@all = []
-
-  def patient(date)
-    @date = date
-    @patient
-    @doctor
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
+  
 end
